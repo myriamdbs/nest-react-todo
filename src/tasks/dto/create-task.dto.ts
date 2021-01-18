325b4d726a76d53c79467b2f5d24399c51601da6
Myriam Dubois <myriam.dubois@skema.edu>
2021-01-18 12:22:00 +0100

    use tasks service into task controller, impliying creating dto

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,6 @@
+export class CreateTaskDto {
+  readonly title: string;
+  readonly description: string;
+  readonly body: string;
+  readonly priority: number;
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TasksService } from './tasks.service';
+import { Task } from './interfaces/task.interface';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private TasksService: TasksService) {}
+
+  @Post()
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    this.TasksService.create(createTaskDto);
+  }
+
   @Get()
-  findAll(): string {
-    return 'This action returns all tasks';
+  async findAll(): Promise<Task[]> {
+    return this.TasksService.findAll();
   }
 }
